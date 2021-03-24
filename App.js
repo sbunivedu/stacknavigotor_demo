a7f0cac219bd5fdd9a56f6d08767a8cb3154bf33
Baochuan Lu <lubaochuan@gmail.com>
2021-03-24 07:13:37 -0500

    going back

--- App.js
+++ App.js
@@ -22,6 +22,18 @@ class DetailsScreen extends React.Component {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.push('Details')}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
       </View>
     );
   }
